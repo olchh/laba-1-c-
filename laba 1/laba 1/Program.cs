cf04897feda4bc7f24ea335d33734225024b45a6
olchh <98604592+olchh@users.noreply.github.com>
2023-02-16 14:34:20 +0800

    Add files via upload

---
+++ laba 1/laba 1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Runtime.Serialization.Formatters;
+
+namespace laba_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число х:");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введие количество шагов:");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            double sum = x;
+
+            for(int i = 1; i <= n; i++)
+            {
+                if (n < 0 && x > 1) {
+                
+                    break;
+                }
+                else {
+                    double a = (2 * i - 1) * Math.Pow(x, 2 * i + 1);
+                    double b = (2 * i) * (2 * i + 1);
+
+                    double a1 = a / b;
+
+                    sum += a1;
+                    Console.WriteLine(sum);
+                }
+            }
+            if (n > 0 && x < 1)
+            {
+                Console.WriteLine( 3.14/2 - sum);
+
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели отрицательное число шагов или не выполнено условие для x < 1.");
+            }
+        }
+    }
+}
